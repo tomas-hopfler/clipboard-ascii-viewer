14a558a8aac07b578844f61158c19f6d9dd01dd2
Tomáš Höpfler <122627736+tomas-hopfler@users.noreply.github.com>
2024-06-21 16:39:22 +0200

    Add files via upload

---
+++ ClipboardImageHelper.cs
@@ -0,0 +1,53 @@
+﻿using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace CIH
+{
+	public class ClipboardImageHelper
+	{
+		[DllImport("user32.dll")]
+		static extern bool OpenClipboard(IntPtr hWndNewOwner);
+
+		[DllImport("user32.dll")]
+		static extern bool CloseClipboard();
+
+		[DllImport("user32.dll")]
+		static extern bool IsClipboardFormatAvailable(uint format);
+
+		[DllImport("user32.dll")]
+		static extern IntPtr GetClipboardData(uint uFormat);
+
+		const uint CF_BITMAP = 2;
+
+		public static Image GetImageFromClipboard()
+		{
+			if (!OpenClipboard(IntPtr.Zero))
+			{
+				Console.WriteLine("Failed to open clipboard.");
+				return null;
+			}
+
+			try
+			{
+				if (!IsClipboardFormatAvailable(CF_BITMAP))
+				{
+					Console.WriteLine("No bitmap in clipboard.");
+					return null;
+				}
+
+				IntPtr handle = GetClipboardData(CF_BITMAP);
+				if (handle == IntPtr.Zero)
+				{
+					Console.WriteLine("Failed to get clipboard data.");
+					return null;
+				}
+
+				return Image.FromHbitmap(handle);
+			}
+			finally
+			{
+				CloseClipboard();
+			}
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿using System.Drawing;
+using CIH;
+
+// load image from clipboard
+Bitmap img = (Bitmap)ClipboardImageHelper.GetImageFromClipboard();
+if (img == null)
+{
+	return;
+}
+
+// characters from lightest to darkest (customizable, array length independent)
+char[] shades = { ' ', '░', '▒', '▓', '█' };
+
+// default resolution settings
+int wMinRes = 50;
+int hMinRes = 50;
+
+// calculate aspect ratio of image
+float ratio = (float)img.Width / img.Height;
+
+// calculate result resolution
+int wRes = Math.Min((int)(Math.Min(wMinRes, img.Height) * ratio), Console.LargestWindowWidth/2);
+int hRes = Math.Min((int)(Math.Min(hMinRes, img.Width) / ratio), Console.LargestWindowHeight);
+
+char[,] arr = new char[wRes, hRes];
+
+// set size of console to size of result image
+Console.SetWindowSize(Math.Min(wRes*2, Console.LargestWindowWidth), hRes);
+
+ShowImage();
+
+void ShowImage()
+{
+	// check for correct image resolution
+	if (img.Width >= wRes && img.Height >= hRes)
+	{
+		Scale();
+	}
+
+	Console.Clear();
+	Console.OutputEncoding = System.Text.Encoding.UTF8;
+	PrintArray();
+
+	// close console with key press
+	Console.ReadKey();
+}
+void Scale()
+{
+	// calculate iteration step for result image
+	float wStep = wRes / (float)img.Width;
+	float hStep = hRes / (float)img.Height;
+
+	for (int h = 0; h < img.Height; h++)
+	{
+		for (int w = 0; w < img.Width; w++)
+		{
+			Color pixel = img.GetPixel(w, h);
+			float brightness = pixel.GetBrightness();
+
+			DrawInArray((int)(w * wStep), (int)(h * hStep), brightness);
+		}
+	}
+}
+void DrawInArray(int x, int y, float pixelBrightness)
+{
+	int shadesCount = shades.Length;
+	int shadeIndex = (int)(pixelBrightness * shadesCount);
+	// get character according to pixel brightness
+	shadeIndex = Math.Clamp(shadeIndex, 0, shadesCount - 1);
+
+	arr[x, y] = shades[shadeIndex];
+}
+void PrintArray()
+{
+	for (int y = 0; y < hRes; y++)
+	{
+		for (int x = 0; x < wRes-1; x++)
+		{
+			// print 2 characters to represent single pixel
+			Console.Write(arr[x, y] + "" + arr[x, y]);
+		}
+		if(y != hRes-1)
+		{
+			Console.Write("\n");
+		}
+	}
+}
